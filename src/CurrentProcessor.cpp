eab4228636fce9711775c5ba6d90776177fc38f6
jiesou <84175239+jiesou@users.noreply.github.com>
2025-03-19 17:44:27 +0800

    feat: 回滚频率计算

--- src/CurrentProcessor.cpp
+++ src/CurrentProcessor.cpp
@@ -7,12 +7,9 @@ void IRAM_ATTR CurrentProcessor::CF1Interrupt() {
   instance->pulse_interval = instance->current_millis -
                              instance->last_pulse_millis; // 两次脉冲的时间差
   instance->last_pulse_millis = instance->current_millis;
-  instance->pulse_count++;
 }
 
-CurrentProcessor::CurrentProcessor() {
-  instance = this;
-}
+CurrentProcessor::CurrentProcessor() { instance = this; }
 
 void CurrentProcessor::begin() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -29,54 +26,15 @@ void CurrentProcessor::begin() {
 // 检测频率
 void CurrentProcessor::update() {
   unsigned long current_millis = millis();
-
-  // 检测长时间无脉冲情况
-  if (current_millis - last_pulse_millis > PULSE_TIMEOUT) {
-    frequency = 0;
-    pulse_interval = 0;
-    last_pulse_millis = current_millis;
-    t_w_sampling_interval = DEFAULT_T_W_SAMPLING_INTERVAL;
-    triggered_t_w_mode = false;
-    return; // 直接返回，防止后续逻辑错误计算频率
+  
+  // 时间已超过上次脉冲间隔 + PULSE_EXTRA_TIMEOUT 认定无脉冲
+  if (current_millis - last_pulse_millis >
+      pulse_interval + PULSE_EXTRA_TIMEOUT) {
+    frequency = 0; // 无脉冲时频率置零
+    return;
   }
 
-  // 计算频率 - 两种方法选一种更适合的，保证实时性、准确性
-
-  // 方法1：基于脉冲间隔（适合 >3.3hz 高频信号）
-  if (pulse_interval < 300) {
-    // 时间窗口 300ms
-    if (current_millis - last_interval_update_time <= 300)
-      return;
-    triggered_t_w_mode = false;
-    t_w_sampling_interval =
-        DEFAULT_T_W_SAMPLING_INTERVAL; // 已是高频，归位默认采样时间，这样下一轮会收敛更快
-    frequency = 1000.0 / pulse_interval;
-  }
-  // 方法2：基于时间窗口（t_w）计数（适合低频信号）
-  else {
-    // 时间窗口 t_w_sampling_interval
-    if (current_millis - last_t_w_update_time <= t_w_sampling_interval)
-      return;
-    last_t_w_update_time = current_millis;
-
-    if (pulse_count == 0) {
-      // 脉冲找不到
-      // 1 ms 1 ms 地尝试查找
-      t_w_sampling_interval = 1;
-      frequency = 0; // 确保无脉冲时频率置零
-      return;
-    }
-    // 如果出现了至少一个脉冲
-    triggered_t_w_mode = true;
-    // 根据两次脉冲时间重设新采样窗口时间
-    t_w_sampling_interval = pulse_interval;
-
-    frequency = (1000.0 / t_w_sampling_interval) * pulse_count;
-  }
-
-  // 每次流程的最后，无论何种采样方式都要置零
-  pulse_count = 0;
-  last_interval_update_time = current_millis;
+  frequency = 1000.0 / pulse_interval;
 
   // 内部计数
   if (frequency > FREQUENCY_THRESHOLD && !appliance_working) {
--- src/CurrentProcessor.h
+++ src/CurrentProcessor.h
@@ -6,26 +6,19 @@
 #define BUTTON_PIN 2
 #define FREQUENCY_THRESHOLD 15.0
 
-// 简单的频率检测相关常量
-#define PULSE_TIMEOUT 1000        // 1000 ms 无脉冲视为停止
-#define DEFAULT_T_W_SAMPLING_INTERVAL 300 // 默认 脉冲间隔 法采样时间
+#define PULSE_EXTRA_TIMEOUT 200        // 1000 ms 无脉冲视为停止
 
 class CurrentProcessor {
 private:
     static CurrentProcessor* instance;
-    volatile unsigned long current_millis = millis();
-    volatile unsigned long last_pulse_millis = 0;
-    volatile unsigned long pulse_interval = 0; // ms
-    volatile unsigned int pulse_count = 0;
-    volatile bool triggered_t_w_mode = false;
-    volatile unsigned int t_w_sampling_interval = 300;
-    volatile unsigned long last_t_w_update_time;
+    volatile unsigned long current_millis;
+    volatile unsigned long last_pulse_millis;
+    volatile unsigned long pulse_interval; // ms
     
     static void IRAM_ATTR CF1Interrupt();
 
 public:
-  unsigned long last_interval_update_time = 0;
-  volatile float frequency = 0;
+  volatile float frequency;
   //  内部计数
   volatile bool appliance_working = false;
   volatile unsigned int screw_count = 4;
